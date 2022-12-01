1a3bb4218b3756099ec7b0640396d11f805e4d04
JorenVC <joren_mathis@hotmail.com>
2022-12-01 18:55:47 +0100

    customers

--- project obj/main.cpp
+++ project obj/main.cpp
@@ -95,9 +95,106 @@ void addhotelroom(int floors) {
 	hotelroom.setNrBeds(nrBeds);
 	hotelroom.setChildrenBeds(childrenBed);
 }
+void CreateCustomer() {
+	Customer customer;
+	std::string naam, adres, email;
+
+	cout << endl << endl << "----------------------------------------------------------------------------" << endl << endl;
+	cout << "You want to create a customer account:" << endl << endl;
+	cout << endl << "What is your name: ";
+	cin >> naam;
+	cout << endl <<  "What is your address: ";
+	cin >> adres;
+	cout << endl << "What is your email address: ";
+	cin >> email;
+
+	customer.setName(naam);
+	customer.setAddress(adres);
+	customer.setMail(email);
+	cout << endl << endl << "----------------------------------------------------------------------------" << endl << endl;
+}
+void ChangeCustomer(std::string cust) {
+	Customer customer;
+	int choise;
+	std::string naam, adres, email;
+
+	cout << endl << endl << "----------------------------------------------------------------------------" << endl << endl;
+	cout << "Your are currently want to change Customer account: " << cust << endl;
+	do {
+		cout << "- 1 : Change Name" << endl << "- 2 : Change address" << endl << "- 3 : Change Email" << endl << "- 0 : Stop" << endl << endl;
+		do {
+			cout << "Choise: ";
+			cin >> choise;
+		} while (choise < 0 or choise > 4);
+		
+
+		if (choise == 1) {
+			cout << "What is the new name: ";
+			cin >> naam;
+			customer.setName(naam);
+			cout << "The name is changed" << endl;
+			cout << endl << endl << "----------------------------------------------------------------------------" << endl << endl;
+		}
+		if (choise == 2) {
+			cout << endl << "What is the new address: ";
+			cin >> adres;
+			customer.setAddress(adres);
+			cout << endl << "The adres is changed" << endl;
+			cout << endl << endl << "----------------------------------------------------------------------------" << endl << endl;
+		}
+		if (choise == 3) {
+			cout << "What is the new Email: ";
+			cin >> email;
+			customer.setMail(email);
+			cout << endl << "The email is changed" << endl;
+			cout << endl << endl << "----------------------------------------------------------------------------" << endl << endl;
+		}
+	} while (choise != 0);
+	cout << endl << endl << "----------------------------------------------------------------------------" << endl << endl;
+}
+void DeleteCustomer(std::string cust) {
+	Customer customer;
+
+
+}
+
+void SearchCustomer() {
+	Customer customer;
+	std::string Scustom = "Joren";
+	int choise;
+
+	cout << endl << endl << "----------------------------------------------------------------------------" << endl << endl;
+	do {
+		cout << "Would you like to Create or Change or Delete a customer: " << endl << endl;
+		cout << "- 1 : Create customer" << endl << "- 2 : Change customer" << endl << "- 3 : Delete customer" << endl << "- 0 : Stop" << endl << endl;
+		do {
+			cout << "Choise: ";
+			cin >> choise;
+		} while (choise < 0 or choise > 4);
+
+
+		if (choise == 1) {
+			CreateCustomer();
+			cout << endl << "Customer has been created!" << endl;
+			cout << endl << endl << "----------------------------------------------------------------------------" << endl << endl;
+		}
+		if (choise == 2) {
+			ChangeCustomer(Scustom);
+			cout << endl << "Customer has been changed!" << endl;
+			cout << endl << endl << "----------------------------------------------------------------------------" << endl << endl;
+		}
+		if (choise == 3) {
+			DeleteCustomer(Scustom);
+			cout << endl << "Customer has been deleted!" << endl;
+			cout << endl << endl << "----------------------------------------------------------------------------" << endl << endl;
+		}
+	} while (choise != 0);
+	cout << endl << endl << "----------------------------------------------------------------------------" << endl << endl;
+}
+
+
 int main() {
-	Cabin cabin;
-	cabin = createCabin(110);
+	SearchCustomer();
 }
 
 Cabin createCabin(int NumberOfRooms) {
@@ -108,10 +205,6 @@ Cabin createCabin(int NumberOfRooms) {
 
 LuxuryLevel LuxuryNiveau(bool BBQ, bool SurroundSystem, bool breakfastService, bool cleaningService, std::string accomodationKind);
 
-int main() {
-	addhotelroom(6);
-	return 0;
-}
 
 LuxuryLevel LuxuryNiveau(bool BBQ, bool SurroundSystem, bool breakfastService, bool cleaningService, std::string accomodationKind)
 {
@@ -126,3 +219,4 @@ LuxuryLevel LuxuryNiveau(bool BBQ, bool SurroundSystem, bool breakfastService, b
 
 	return LuxuryLevel();
 }
+
